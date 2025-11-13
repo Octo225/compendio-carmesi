34dc0abd3c41d7816604551dd906f15ef1fb5295
Octo225 <luciosgonzalea@gmail.com>
2025-11-13 10:37:32 -0600

    configuracion firebase

--- src/environments/firebaseconfig.ts
+++ src/environments/firebaseconfig.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBHjXGQ21TVC-dHAz3g8Hnx2sp-iyoUneo",
-    authDomain: "practica10a-efad8.firebaseapp.com",
-    projectId: "practica10a-efad8",
-    storageBucket: "practica10a-efad8.firebasestorage.app",
-    messagingSenderId: "190684652817",
-    appId: "1:190684652817:web:df51c318fe95606d5598b0",
-    measurementId: "G-P7P5XFY7L7"
-  };
+  apiKey: "AIzaSyD_FxZ2lMUEMXeCufgp3XadDJkwCrC659A",
+  authDomain: "compendio-carmesi-3c0cc.firebaseapp.com",
+  projectId: "compendio-carmesi-3c0cc",
+  storageBucket: "compendio-carmesi-3c0cc.firebasestorage.app",
+  messagingSenderId: "880637946688",
+  appId: "1:880637946688:web:aa3bd8aef694e8396028a5"
+};
